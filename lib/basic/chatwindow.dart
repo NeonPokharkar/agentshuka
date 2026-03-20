f2d7fa8de89730d68bab52da904dc4e1b7a4858a
NeonPokharkar <neonpokharkar@gmail.com>
2026-03-20 23:10:00 +0530

    Setting bottom area

--- lib/basic/chatwindow.dart
+++ lib/basic/chatwindow.dart
@@ -32,66 +32,68 @@ class _ChatWindowState extends State<ChatWindow> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              padding: EdgeInsets.all(10),
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final msg = messages[index];
-                return Messagebubble(text: msg["text"], isMe: msg["isMe"]);
-              },
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(10),
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  final msg = messages[index];
+                  return Messagebubble(text: msg["text"], isMe: msg["isMe"]);
+                },
+              ),
             ),
-          ),
-          TextInput(
-            processInput: (str) async {
-              setState(() {
-                messages.add({"text":str, "isMe":true});
-              });
-
-              final queryParams = {
-                "chat" : str
-              };
-              final uri = Uri.http("100.72.140.76:8000","/query", queryParams);
-              try {
-                final request = await http.Request('GET', uri);
-                request.headers.addAll({
-                  'Connection': 'Keep-Alive',
-                  'Keep-Alive': 'timeout=1000, max=1000'
+            TextInput(
+              processInput: (str) async {
+                setState(() {
+                  messages.add({"text":str, "isMe":true});
                 });
-                final response = await http.Client().send(request);
 
-                if(response.statusCode == 200)
-                {
-                  var message = "";
-
-                  response.stream.listen((value) {
-                    final chunk = String.fromCharCodes(value);
-                    print("Recieved : $chunk");
-                    message+=chunk;
-                  }).onDone(() {
-                    setState(() {
-                      messages.add({"text": message, "isMe":false});
-                    });
+                final queryParams = {
+                  "chat" : str
+                };
+                final uri = Uri.http("100.72.140.76:8000","/query", queryParams);
+                try {
+                  final request = await http.Request('GET', uri);
+                  request.headers.addAll({
+                    'Connection': 'Keep-Alive',
+                    'Keep-Alive': 'timeout=1000, max=1000'
                   });
+                  final response = await http.Client().send(request);
 
-                } else {
-                  print("Request Failed");
+                  if(response.statusCode == 200)
+                  {
+                    var message = "";
+
+                    response.stream.listen((value) {
+                      final chunk = String.fromCharCodes(value);
+                      print("Recieved : $chunk");
+                      message+=chunk;
+                    }).onDone(() {
+                      setState(() {
+                        messages.add({"text": message, "isMe":false});
+                      });
+                    });
+
+                  } else {
+                    print("Request Failed");
+                    setState(() {
+                      messages.add({"text": "Some error occurred on server : ${response.statusCode}", "isMe":false});
+                    });
+                  }
+                } catch (e) {
+                  print("Error : $e");
                   setState(() {
-                    messages.add({"text": "Some error occurred on server : ${response.statusCode}", "isMe":false});
+                    messages.add({"text": "Some error occurred : ${e}", "isMe":false});
                   });
                 }
-              } catch (e) {
-                print("Error : $e");
-                setState(() {
-                  messages.add({"text": "Some error occurred : ${e}", "isMe":false});
-                });
-              }
-            },
-          )
-        ],
+              },
+            )
+          ],
+        ),
       ),
     );
   }

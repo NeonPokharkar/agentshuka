b0ad843bf13b384b7ba644b8d5ebfd2f85c1de5a
NeonPokharkar <neonpokharkar@gmail.com>
2026-04-05 16:36:07 +0530

    Removing Fluff

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
-import 'package:agentshuka/mobile/ui/chatwindow.dart';
 import 'package:agentshuka/shared/colors/colors.dart';
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:agentshuka/wear/ui/agentscreen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wear_os_connectivity/flutter_wear_os_connectivity.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await AndroidAlarmManager.initialize();
 
   runApp(MainApp());
 
@@ -16,9 +13,7 @@ Future<void> main() async {
 
   await flutterWearOsConnectivity.configureWearableAPI();
 
-  await flutterWearOsConnectivity.registerNewCapability("shuka_phone");
-
-  await appPermissions.request();
+  await flutterWearOsConnectivity.registerNewCapability("shuka_wear");
 }
 
 class MainApp extends StatefulWidget {
@@ -33,14 +28,12 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shuka',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: colorThemeCurrent.colors.primaryColor),
-      ),
-      home: ChatWindow()
+        debugShowCheckedModeBanner: false,
+        title: 'Shuka',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: colorThemeCurrent.colors.primaryColor),
+        ),
+        home: Agentscreen()
     );
   }
 }
-
-List<Permission> appPermissions = [Permission.microphone, Permission.notification, Permission.scheduleExactAlarm, Permission.ignoreBatteryOptimizations];
